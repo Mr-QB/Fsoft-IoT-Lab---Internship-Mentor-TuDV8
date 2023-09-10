fb216f94de79482b669e1d6b179fbca258ee6867
Nguyen Quoc Bao <20020632@vnu.edu.vn>
2023-09-11 06:52:06 +0700

    Update ble_app.py

--- ble_app/ble_app.py
+++ ble_app/ble_app.py
@@ -7,20 +7,28 @@ from itertools import count
 
 time_points = []
 value_data = []
+
+
 def hex2int(hex_string):
     try:
-        uint8_value = int(hex_string[:2], 16)
-        return uint8_value
+        result = (int(hex_string[6:8], 16) << 16) | (int(hex_string[8:10], 16) << 8) | int(hex_string[10:12], 16)
+        result = result << 12
+        result = result >> 12
+        result = ((result) / 1048576 * 200 - 50)
+        return result
     except ValueError:
         print("Error convert")
 
+
+
+last_value = None
 def get_data():
     ws = websocket.WebSocket()
     ws.connect("ws://raspberrypi:8080")
     command = {}
     command['command'] = 1
-    command['bdaddr'] = "C8:F0:9E:9A:54:FA"
-    command['handle'] = "/org/bluez/hci0/dev_C8_F0_9E_9A_54_FA/service0028/char0029"
+    command['bdaddr'] = "60:A4:23:D4:5C:9C"
+    command['handle'] = "/org/bluez/hci0/dev_60_A4_23_D4_5C_9C/service0001/char0002"
     ws.send(json.JSONEncoder().encode(command))
     print(ws.recv())
     while True:
@@ -28,24 +36,30 @@ def get_data():
         print(hex2int(data))
         # Plot data
         def animate(i):
-            int_data = hex2int(json.loads(ws.recv())['value'])
-            time_points.append(i)  # Assuming you want to use an incremental time axis
-            value_data.append(int_data)
+            data = json.loads(ws.recv())['value']
+            int_data = hex2int(data)
+            global last_value
+            last_value = None
+            if last_value != int_data:
+                print(int_data, json.loads(ws.recv())['value'])
+                time_points.append(i)  # Assuming you want to use an incremental time axis
+                value_data.append(int_data)
 
-            # Limit the number of points displayed if you don't want to keep all data
-            max_points = 50  # Adjust as needed
-            if len(time_points) > max_points:
-                del time_points[0]
-                del value_data[0]
+                # Limit the number of points displayed if you don't want to keep all data
+                max_points = 5000000  # Adjust as needed
+                if len(time_points) > max_points:
+                    del time_points[0]
+                    del value_data[0]
 
-            # Clear the previous plot and plot the updated data
-            plt.cla()
-            plt.plot(time_points, value_data, label='Data')
-            plt.xlabel('Time')
-            plt.ylabel('Value')
-            plt.legend()
+                # Clear the previous plot and plot the updated data
+                plt.cla()
+                plt.plot(time_points, value_data, label='Data')
+                plt.xlabel('Time')
+                plt.ylabel('Value')
+                plt.legend()
+                last_value = int_data
 
-        ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+        ani = FuncAnimation(plt.gcf(), animate, interval=1500)
         plt.show()
 
 get_data()
